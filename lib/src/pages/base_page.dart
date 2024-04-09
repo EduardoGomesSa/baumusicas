37b66f5545da5af5bb3739711e7a071922ad3909
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2024-04-09 18:39:24 -0300

    criação do sidebar, arquivos home e playlist

---
+++ lib/src/pages/base_page.dart
@@ -0,0 +1,48 @@
+import 'package:baumusicas/src/pages/home/home_page.dart';
+import 'package:baumusicas/src/pages/playlist/playlist_page.dart';
+import 'package:flutter/material.dart';
+
+class BasePage extends StatefulWidget {
+  const BasePage({super.key});
+
+  @override
+  State<BasePage> createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
+  int currentIndex = 0;
+  final pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: const [
+          HomePage(),
+          PlaylistPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: (index){
+          setState(() {
+            currentIndex = index;
+            pageController.jumpToPage(index);
+          });
+        },
+        backgroundColor: Colors.brown,
+        selectedItemColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todas'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.playlist_add),
+            label: 'Playlists'
+          )
+        ],
+      ),
+    );
+  }
+}
