2229425cf6f83fc1f0a22c99ed23b42ee175f42a
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-07-29 22:30:55 -0300

    feat: :sparkles: change title the project

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Baum Musicas',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
