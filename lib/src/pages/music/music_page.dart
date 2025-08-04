19e0cb212e6542f0a54633a1a8ea75e13bbf1ae6
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-08-04 01:02:38 -0300

    feat: :sparkles: import controll into MusicPage

--- lib/src/pages/music/music_page.dart
+++ lib/src/pages/music/music_page.dart
@@ -1,4 +1,7 @@
+import 'package:baumusicas/src/controllers/music_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
 class MusicPage extends StatelessWidget {
@@ -8,6 +11,7 @@ class MusicPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final MusicController controller = Get.find();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Detalhes da música"),
@@ -36,12 +40,20 @@ class MusicPage extends StatelessWidget {
                         Icons.skip_previous_rounded,
                         size: 50,
                       )),
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.play_arrow,
-                        size: 50,
-                      )),
+                  Obx(
+                    () => IconButton(
+                        onPressed: () {
+                          controller.isPlaying.value
+                              ? controller.pauseMusic()
+                              : controller.playMusic(model);
+                        },
+                        icon: Icon(
+                          controller.isPlaying.value
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                          size: 50,
+                        )),
+                  ),
                   IconButton(
                       onPressed: () {},
                       icon: const Icon(
