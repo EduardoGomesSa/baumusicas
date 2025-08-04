894ec2e294e6c42a12bc736c85e83be3b0fd46b3
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-08-04 00:47:12 -0300

    feat: :sparkles: menu of status created

--- lib/src/pages/music/music_page.dart
+++ lib/src/pages/music/music_page.dart
@@ -12,7 +12,49 @@ class MusicPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Detalhes da música"),
       ),
-      body: Center(child: Text(model.title),),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: double.infinity,
+              color: Colors.red,
+              child: Text("Imagem"),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(model.title),
+              Text(model.artist ?? "Artista Desconhecido"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.skip_previous_rounded,
+                        size: 50,
+                      )),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.play_arrow,
+                        size: 50,
+                      )),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.skip_next_rounded,
+                        size: 50,
+                      )),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(height: 20)
+        ],
+      ),
     );
   }
 }
