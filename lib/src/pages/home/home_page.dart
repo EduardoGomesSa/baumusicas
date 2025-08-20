11fac4576745d2a43d0e3b60a3df66806b48b68a
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-08-19 23:51:45 -0300

    feat: :sparkles: adding SafeArea in HomePage

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -10,101 +10,104 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todas as Músicas'),
-      ),
-      body: Obx(() {
-        if (controller.isLoading.value) {
-          return const Center(child: CircularProgressIndicator());
-        }
-
-        if (controller.musics.isEmpty) {
-          return const Center(child: Text("Nenhuma música encontrada."));
-        }
-        return ListView.builder(
-          itemCount: controller.musics.length,
-          itemBuilder: (context, index) {
-            final song = controller.musics[index];
-            return GestureDetector(
-              onTap: () {
-                controller.playMusic(index);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => MusicPage(index: index)));
-              },
-              child: ListTile(
-                  title: Text(song.title),
-                  subtitle: Text(song.artist ?? "Artista desconhecido"),
-                  trailing: Obx(() => IconButton(
-                        icon: Icon(controller.isPlaying.value &&
-                                controller.currentSong.value == song
-                            ? Icons.pause
-                            : Icons.play_arrow),
-                        onPressed: () => controller.isPlaying.value &&
-                                controller.currentIndex.value == index
-                            ? controller.pauseMusic()
-                            : controller.playMusic(index),
-                      ))),
-            );
-          },
-        );
-      }),
-      bottomNavigationBar: Obx(() {
-        final song = controller.currentSong.value;
-        final pos = controller.position.value;
-        final dur = controller.duration.value;
-        if (song == null) return const SizedBox.shrink();
-
-        return GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) =>
-                    MusicPage(index: controller.currentIndex.value)));
-          },
-          child: SizedBox(
-            width: double.infinity,
-            height: 120,
-            child: Column(
-              children: [
-                ListTile(
-                  title: Text(song.title),
-                  subtitle: Text(song.artist ?? ""),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        onPressed: controller.pauseMusic,
-                        icon: Icon(controller.isPlaying.value
-                            ? Icons.pause
-                            : Icons.play_arrow),
-                      ),
-                      controller.isPlaying.value
-                          ? const SizedBox.shrink()
-                          : IconButton(
-                              onPressed: controller.stopMusic,
-                              icon: const Icon(Icons.stop),
-                            )
-                    ],
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Todas as Músicas'),
+        ),
+        body: Obx(() {
+          if (controller.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          }
+      
+          if (controller.musics.isEmpty) {
+            return const Center(child: Text("Nenhuma música encontrada."));
+          }
+          return ListView.builder(
+            itemCount: controller.musics.length,
+            itemBuilder: (context, index) {
+              final song = controller.musics[index];
+              return GestureDetector(
+                onTap: () {
+                  controller.playMusic(index);
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => MusicPage(index: index)));
+                },
+                child: ListTile(
+                    title: Text(song.title),
+                    subtitle: Text(song.artist ?? "Artista desconhecido"),
+                    trailing: Obx(() => IconButton(
+                          icon: Icon(controller.isPlaying.value &&
+                                  controller.currentSong.value == song
+                              ? Icons.pause
+                              : Icons.play_arrow),
+                          onPressed: () => controller.isPlaying.value &&
+                                  controller.currentIndex.value == index
+                              ? controller.pauseMusic()
+                              : controller.playMusic(index),
+                        ))),
+              );
+            },
+          );
+        }),
+        bottomNavigationBar: Obx(() {
+          final song = controller.currentSong.value;
+          final pos = controller.position.value;
+          final dur = controller.duration.value;
+          if (song == null) return const SizedBox.shrink();
+      
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) =>
+                      MusicPage(index: controller.currentIndex.value)));
+            },
+            child: SizedBox(
+              width: double.infinity,
+              height: 120,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(song.title),
+                    subtitle: Text(song.artist ?? ""),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: controller.pauseMusic,
+                          icon: Icon(controller.isPlaying.value
+                              ? Icons.pause
+                              : Icons.play_arrow),
+                        ),
+                        controller.isPlaying.value
+                            ? const SizedBox.shrink()
+                            : IconButton(
+                                onPressed: controller.stopMusic,
+                                icon: const Icon(Icons.stop),
+                              )
+                      ],
+                    ),
                   ),
-                ),
-                SliderTheme(
-                  data: SliderThemeData(
-                    thumbShape: SliderComponentShape.noThumb,
-                    trackHeight: 1.0,
+                  SliderTheme(
+                    data: SliderThemeData(
+                      thumbShape: SliderComponentShape.noThumb,
+                      trackHeight: 1.0,
+                    ),
+                    child: Slider(
+                      min: 0.0,
+                      max: dur.inMilliseconds.toDouble(),
+                      value: pos.inMilliseconds.toDouble(),
+                      onChanged: (value) {},
+                      allowedInteraction: SliderInteraction.slideOnly,
+                    ),
                   ),
-                  child: Slider(
-                    min: 0.0,
-                    max: dur.inMilliseconds.toDouble(),
-                    value: pos.inMilliseconds.toDouble(),
-                    onChanged: (value) {},
-                    allowedInteraction: SliderInteraction.slideOnly,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      }),
+          );
+        }),
+      ),
     );
   }
 }
